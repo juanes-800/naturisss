3e5eff5ac6040010a639c915ffedcd86b0c98011
juan esteban <juanealzate800@gmail.com>
2022-10-27 19:16:55 -0500

    loading

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,28 @@
 import { useCartContext } from "../context/CartContext";
 import { NavLink } from 'react-router-dom';
 import ItemCart from "./ItemCart";
+import {addDoc, collection, getFirestore} from 'firebase/firestore';
 const Cart = () =>{
-    const{cart, totalPrice} = useCartContext();
+    const{cart, totalPrice} = useCartContext(); 
+
+    const order = {
+        buyer:{
+            name: 'juan esteban Alzate',
+            email: 'juanealzate@800gmail.com',
+            phone:'3197416825',
+            address:'crr24#9a-35'
+        },
+        items: cart.map(product =>({id: product.id, name: product.name, price: product.price, cantidad:product.cantidad})) ,
+        total: totalPrice(),
+    }
+
+    const handleClick = () =>{
+        const db = getFirestore();
+        const ordersCollection = collection(db, 'orders');
+        addDoc(ordersCollection, order)
+         .then(({id}) => console.log(id))
+    }
+    console.log(order)
    
     if(cart.length === 0){
         return(
@@ -18,6 +38,7 @@ const Cart = () =>{
             cart.map(product => <ItemCart key={product.id} product={product}/>)
         }
         <p> total: {totalPrice()}</p>
+        <button onClick={handleClick}>Generar Compra</button>
 
         </>
     )
